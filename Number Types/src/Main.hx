be35b6715a1cb3b54979f3ffa472f36c55bd22ba
Kent Hinson <kent@kenthinson.com>
2018-11-17 22:56:33 -0800

    Add docs to Numbers

---
+++ Number Types/src/Main.hx
@@ -0,0 +1,9 @@
+class Main {
+	static function main() {
+		var ourFirstNumber;
+		ourFirstNumber = 5;
+		ourFirstNumber = -100;
+		ourFirstNumber = 1000000;
+		trace(ourFirstNumber);
+	}
+}
--- Numbers/src/Main.hx
+++ Numbers/src/Main.hx
@@ -1,9 +1,19 @@
 class Main {
 	static function main() {
+
+		//Create ourFirstNumber variable
 		var ourFirstNumber;
+
+		//Add the value of 5 to ourFirstNumber
 		ourFirstNumber = 5;
+
+		//Change (overwrite) the value from 5 to -100
 		ourFirstNumber = -100;
+
+		//Change (overwrite) the value from -100 to 1000000
 		ourFirstNumber = 1000000;
+
+		//Output the data inside ourFirstNumber to the screen
 		trace(ourFirstNumber);
 	}
 }
